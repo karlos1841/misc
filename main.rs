3c372bfb67fedd9131b9d9c8c07036a0b230133b
karlos1841 <karlos1841@gmail.com>
2018-11-14 13:01:42 +0100

    Add files via upload

--- main.rs
+++ main.rs
@@ -3,17 +3,18 @@ use std::io::Write;
 use std::fs::File;
 use std::fmt;
 use std::net::{TcpListener, TcpStream};
-use std::time::Duration;
-use std::thread;
+//use std::time::Duration;
+//use std::thread;
 use std::io::BufReader;
+use std::process::Command;
 
 fn main()
 {
-    let mut f = File::open("/home/karol/Downloads/index.html").unwrap();
+    let mut f = File::open("index.html").unwrap();
     let mut index_buffer = String::new();
     f.read_to_string(&mut index_buffer).unwrap();
 
-    let mut f2 = File::open("/home/karol/Downloads/index2.html").unwrap();
+    let mut f2 = File::open("index2.html").unwrap();
     let mut index2_buffer = String::new();
     f2.read_to_string(&mut index2_buffer).unwrap();
 
@@ -25,78 +26,76 @@ fn main()
         {
             Ok((stream, addr)) => 
             {
-                println!("New client: {}", addr);
-                let index_buffer_clone = index_buffer.clone();
-                let index2_buffer_clone = index2_buffer.clone();
-                thread::spawn(|| {
-                    handle_connection(stream, index_buffer_clone, index2_buffer_clone);
-                });
+                let mut address = String::new();
+                fmt::write(&mut address, format_args!("{}", addr)).unwrap();
+                println!("New client: {}", address);
+                //let index_buffer_clone = index_buffer.clone();
+                //let index2_buffer_clone = index2_buffer.clone();
+                //thread::spawn(|| {
+                    let data: String = handle_connection(stream, &index_buffer, &index2_buffer);
+                    send_mail(&address, &data, &"mail");
+                //});
             },
             Err(e) => println!("could not get a client: {}", e)
         }
     }
 }
 
-fn handle_connection(stream: TcpStream, index: String, index2: String)
+fn send_mail(address: &String, data: &String, recip: &str)
+{
+    if !data.is_empty()
+    {
+        let mut formatted_output = String::new();
+        fmt::write(&mut formatted_output, format_args!("echo \"Adres klienta: {}\n{}\n\"|mail -s formularz {}", address, data, recip)).unwrap();
+        let mut mail_cmd = Command::new("/bin/sh");
+        mail_cmd.arg("-c")
+                .arg(&formatted_output)
+                .spawn()
+                .unwrap();
+    }
+}
+
+fn handle_connection(stream: TcpStream, index: &String, index2: &String) -> String
 {
     // panic if cannot set stream as nonblocking
     //stream.set_nonblocking(true).unwrap();
 
-    // gathering requests from client
     // 1 second timeout
     //let timeout: Option<Duration> = Some(Duration::new(1, 0));
     //stream.set_read_timeout(timeout).unwrap();
 
-    let mut client_response = String::new();
+    let mut client_response_header = String::new();
     let mut stream_buf = BufReader::new(&stream);
     loop
     {
         let mut tmp = String::new();
         stream_buf.read_line(&mut tmp).unwrap();
-        client_response.push_str(&tmp);
-        if tmp == "\r\n"
-        {
-            println!("{}", client_response);
-            break;
-        }
+        client_response_header.push_str(&tmp);
+        if tmp == "\r\n"{break;}
     }
-    println!("{}", client_response);
-    let data_length: u32 = 0;
-    let mut data_str: &str;
-    if client_response.contains("Content-Length: ")
+    // client_response_header contains http header now
+    println!("{}", client_response_header);
+
+    let mut client_response_data = String::new();
+    if client_response_header.contains("Content-Length: ")
     {
-        let content_length: Vec<&str> = client_response.rsplit("Content-Length: ").collect();
+        let content_length: Vec<&str> = client_response_header.rsplit("Content-Length: ").collect();
         let content_length: Vec<&str> = content_length[0].split("\r\n").collect();
-        let data_length: usize = content_length[0].parse().unwrap();
-        let mut data_buffer = vec![0; data_length];
+        let client_response_data_length: usize = content_length[0].parse().unwrap();
+        let mut data_buffer: Vec<u8> = vec![0; client_response_data_length];
         stream_buf.read_exact(&mut data_buffer).unwrap();
-        match std::str::from_utf8(&data_buffer)
+        match String::from_utf8(data_buffer)
         {
-            Ok(s) => {},//{data_str = s;},
+            Ok(s) => {client_response_data.push_str(&s);},
             Err(_) => {}
         }
     }
-    println!("{}", data_length);
-    println!("{:?}", data_str);
+    // client_response_data contains data after http header
+    println!("{}", client_response_data);
 
-    // take first not GET client request(POST) and split by \r\n\r\n so that data is the last element
-    let data: Vec<&str> = client_response.split("GET").collect();
-    let data: Vec<&str> = data[0].split("\r\n\r\n").collect();
-
-    let mut result = String::new();
-    if data.last() != Some(&"")
-    {
-        match data.last()
-        {
-            Some(ref x) => {
-                result.push_str(x);
-            },
-            None => {}
-        }
-    }
-    result = result.replace("&", "\n");
-    let result: Vec<&str> = result.lines().collect();
-    println!("{:?}", result.last());
+    client_response_data = client_response_data.replace("&", "\n");
+    let result: Vec<&str> = client_response_data.lines().collect();
+    //println!("{:?}", result.last());
     if result.last() == Some(&"dalej=Dalej")
     {
         // send next page
@@ -107,6 +106,8 @@ fn handle_connection(stream: TcpStream, index: String, index2: String)
         // send first page to client
         send_response(&stream, &index);
     }
+
+    return client_response_data.clone();
 }
 
 fn send_response(mut stream: &TcpStream, data: &String)
