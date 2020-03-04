d656901d9951ce5bd722bd130ec7411f1336df17
karlos1841 <karlos1841@gmail.com>
2020-03-04 13:58:10 +0100

    Add files via upload

--- dynArr.h
+++ dynArr.h
@@ -3,25 +3,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-// initialize to zero
+#include <stdint.h>
+
+#define DynBuffer 100
+
+// internal struct, do not touch
 typedef struct DynArr
 {
-    char *ptr; // 1 byte wide
-    size_t NUMBER_OF_ELEMENTS;
-    size_t ELEMENT_SIZE;
-
+    char *DynPtr;
+    size_t DynCapacity;
+    size_t DynENumber;
+    size_t *DynESize;
 } DynArr;
 
-void dynArrCharInit(DynArr *d, const char *e);
-void dynArrCharAdd(DynArr *d, const char *e); // can be only used after dynArrCharInit
-void dynArrStrAdd(DynArr *d, const char *e);
+// initialize DynArr, 0 on success
+uint8_t DynArrInit(DynArr *d, size_t capacity);
 
-void dynArrIntInit(DynArr *d, int e);
-void dynArrIntAdd(DynArr *d, int e); // can be only used after dynArrIntInit
+// destroy DynArr
+void DynArrDestroy(DynArr *d);
 
-void dynArrDestroy(DynArr *d);
-void *dynArrAt(const DynArr *d, size_t index);
-void *dynArrStrAt(const DynArr *d, size_t index);
-void dynArrClear(DynArr *d); // clear content, size of memblock is untouched
+// get size
+size_t DynArrGetSize(DynArr *d);
+
+// add string to DynArr
+void DynArrStrAdd(DynArr *d, const char *e);
+
+// get dynamically allocated value at index, first element is at index 0, on error NULL is returned
+char *DynArrStrAt(const DynArr *d, size_t index);
+
+// add int to DynArr
+void DynArrIntAdd(DynArr *d, int64_t e);
+
+// get int value at index, first element is at index 0, on error 0 is returned
+int64_t DynArrIntAt(const DynArr *d, size_t index);
 
 #endif
